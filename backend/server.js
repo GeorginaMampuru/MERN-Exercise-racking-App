745fa226100fedbe48bd4466bd0ae9132d6e361a
GeorginaMampuru <georginamampuru@gmail.com>
2023-10-09 16:36:44 +0200

    nodemon updated

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,14 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
-
-app.listen(port,
-   () => {
-  console.log(`Server is running on port: ${port}`);
-});
+//servr opeation 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+app.listen(port,
+   () => {
+  console.log(`Server is running on port: ${port}`);
+});
